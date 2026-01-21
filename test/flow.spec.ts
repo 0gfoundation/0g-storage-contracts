bdcfb86017af3c7d788d21012dbb76784f68452a
Peter Zhang <peter@0g.ai>
2026-01-21 17:16:14 +0800

    fix test

--- test/flow.spec.ts
+++ test/flow.spec.ts
@@ -5,64 +5,82 @@ import { Flow } from "../typechain-types";
 
 describe("ZeroGStorage Flow", function () {
     let flow: Flow;
+    let submitter: string;
 
     before(async () => {
         await deployments.fixture("no-market");
 
         flow = await getTypedContract(hre, CONTRACTS.Flow);
+        const { deployer } = await hre.getNamedAccounts();
+        submitter = deployer;
     });
 
     it("submit 256 sectors, in segment #0", async () => {
-        const root = Buffer.from("ccc2460186f7233c927e7db2dcc703c0e500b653ca82273b7bfad8045d85a470", "hex");
+        const root = "0xccc2460186f7233c927e7db2dcc703c0e500b653ca82273b7bfad8045d85a470";
         const result = await flow.submit.staticCall({
-            length: 256 * 256,
-            tags: Buffer.from(""),
-            nodes: [{ root, height: 8 }],
+            data: {
+                length: 256 * 256,
+                tags: "0x",
+                nodes: [{ root, height: 8 }],
+            },
+            submitter,
         });
         expect(result[0]).to.deep.eq(0n);
         expect(result[2]).to.deep.eq(256n);
         expect(result[3]).to.deep.eq(256n);
-        await flow.submit({ length: 256 * 256, tags: Buffer.from(""), nodes: [{ root, height: 8 }] });
+        await flow.submit({
+            data: { length: 256 * 256, tags: "0x", nodes: [{ root, height: 8 }] },
+            submitter,
+        });
     });
 
     it("submit 960 sectors, pad to segment #1", async () => {
-        const root = Buffer.from("ccc2460186f7233c927e7db2dcc703c0e500b653ca82273b7bfad8045d85a470", "hex");
+        const root = "0xccc2460186f7233c927e7db2dcc703c0e500b653ca82273b7bfad8045d85a470";
         const result = await flow.submit.staticCall({
-            length: 960 * 256,
-            tags: Buffer.from(""),
-            nodes: [
-                { root, height: 9 },
-                { root, height: 8 },
-                { root, height: 7 },
-                { root, height: 6 },
-            ],
+            data: {
+                length: 960 * 256,
+                tags: "0x",
+                nodes: [
+                    { root, height: 9 },
+                    { root, height: 8 },
+                    { root, height: 7 },
+                    { root, height: 6 },
+                ],
+            },
+            submitter,
         });
         expect(result[0]).to.deep.eq(1n);
         expect(result[2]).to.deep.eq(1024n);
         expect(result[3]).to.deep.eq(960n);
         await flow.submit({
-            length: 960 * 256,
-            tags: Buffer.from(""),
-            nodes: [
-                { root, height: 9 },
-                { root, height: 8 },
-                { root, height: 7 },
-                { root, height: 6 },
-            ],
+            data: {
+                length: 960 * 256,
+                tags: "0x",
+                nodes: [
+                    { root, height: 9 },
+                    { root, height: 8 },
+                    { root, height: 7 },
+                    { root, height: 6 },
+                ],
+            },
+            submitter,
         });
     });
 
     it("submit 960 sectors, pad to segment #2", async () => {
-        const root = Buffer.from("ccc2460186f7233c927e7db2dcc703c0e500b653ca82273b7bfad8045d85a470", "hex");
+        const root = "0xccc2460186f7233c927e7db2dcc703c0e500b653ca82273b7bfad8045d85a470";
         const result = await flow.submit.staticCall({
-            length: 960 * 256,
-            tags: Buffer.from(""),
-            nodes: [
-                { root, height: 9 },
-                { root, height: 8 },
-                { root, height: 7 },
-                { root, height: 6 },
-            ],
+            data: {
+                length: 960 * 256,
+                tags: "0x",
+                nodes: [
+                    { root, height: 9 },
+                    { root, height: 8 },
+                    { root, height: 7 },
+                    { root, height: 6 },
+                ],
+            },
+            submitter,
         });
         expect(result[0]).to.deep.eq(2n);
         expect(result[2]).to.deep.eq(2048n);
